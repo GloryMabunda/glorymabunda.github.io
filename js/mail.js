dca9eec70ab9f4efd2ef0d2e132da41aa04b86bd
Glory Mabunda <31282200+GloryMabunda@users.noreply.github.com>
2021-10-27 12:20:42 +0200

    Update mail.js

--- js/mail.js
+++ js/mail.js
@@ -1,59 +1,11 @@
-$(document).ready(function() {
-  var script_url = 'https://script.google.com/macros/s/AKfycbyk7A5DxkusIvIcTpT_pECA6QJb3JVDOLg7YWGo2RqL_oQfJ0DDRlVWmxUYdREvVCHBaw/exec';
-  var request;
-
-  if (request) {
-     request.abort();
-  }
-
-  $('#gform').submit(function(event){
-    // setup some local variables
-    var $form = $(this);
-		// let's select and cache all the fields
-		var $inputs = $form.find("input, select, button, textarea");
-		// serialize the data in the form
-		var serializedData = $form.serialize();
-    // let's disable the inputs for the duration of the ajax request
-		// Note: we disable elements AFTER the form data has been serialized.
-		// Disabled form elements will not be serialized.
-    $inputs.prop("disabled", true);
-    //Can fire off the request to /form.php
-    request =
+$("#sendMessage").on("click", function() {
+    message = $("#gform").serialize();
     $.ajax({
-			url: script_url,
-			type: "POST",
-			data: serializedData
+        url: "//formspree.io/sgmabunda@gmail.com", 
+        method: "POST",
+        data: {message: message },
+        dataType: "json"
     });
-
-    // callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-      // log a message to the console
-      //$('#success').show();
-      swal({
-    		title: "Sweet!",
-    		text: "Your message has been sent."
-    	});
-      console.log("Contact form submission handler loaded successfully.");
-    });
-    // callback handler that will be called on failure
-    request.fail(function (jqXHR, textStatus, errorThrown){
-      // log the error to the console
-      //$('#failed').show();
-      swal({
-        title: "Oops!",
-        text: "Something happened, try submitting your response again."
-      });
-      console.log("The following error occured: "+ textStatus, errorThrown);
-    });
-    // callback handler that will be called regardless
-    // if the request failed or succeeded
-    request.always(function () {
-      // reenable the inputs
-      $inputs.prop("disabled", false);
-    });
-
-    // prevent default posting of form
-    event.preventDefault();
-
-  });
+    alert('Message submitted. Thanks for the message.');
+    return false;
 });
